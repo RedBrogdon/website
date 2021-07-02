ae853046ab56cd17df5db30684f0c165179b5d9b
Andrew Brogdon <redbrogdon@google.com>
2021-07-02 12:35:40 -0700

    Updating analysis options, formatting

--- examples/_animation/hero_animation/main.dart
+++ examples/_animation/hero_animation/main.dart
@@ -56,28 +56,27 @@ class HeroAnimation extends StatelessWidget {
           photo: 'images/flippers-alpha.png',
           width: 300.0,
           onTap: () {
-            Navigator.of(context).push(MaterialPageRoute<void>(
-              builder: (BuildContext context) {
-                return Scaffold(
-                  appBar: AppBar(
-                    title: const Text('Flippers Page'),
+            Navigator.of(context)
+                .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: const Text('Flippers Page'),
+                ),
+                body: Container(
+                  // Set background to blue to emphasize that it's a new route.
+                  color: Colors.lightBlueAccent,
+                  padding: const EdgeInsets.all(16.0),
+                  alignment: Alignment.topLeft,
+                  child: PhotoHero(
+                    photo: 'images/flippers-alpha.png',
+                    width: 100.0,
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
                   ),
-                  body: Container(
-                    // Set background to blue to emphasize that it's a new route.
-                    color: Colors.lightBlueAccent,
-                    padding: const EdgeInsets.all(16.0),
-                    alignment: Alignment.topLeft,
-                    child: PhotoHero(
-                      photo: 'images/flippers-alpha.png',
-                      width: 100.0,
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ),
-                );
-              }
-            ));
+                ),
+              );
+            }));
           },
         ),
       ),
--- examples/_animation/radial_hero_animation/main.dart
+++ examples/_animation/radial_hero_animation/main.dart
@@ -16,7 +16,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart' show timeDilation;
 
 class Photo extends StatelessWidget {
-  Photo({ Key? key, required this.photo, this.onTap }) : super(key: key);
+  Photo({Key? key, required this.photo, this.onTap}) : super(key: key);
 
   final String photo;
   final VoidCallback? onTap;
@@ -45,8 +45,8 @@ class RadialExpansion extends StatelessWidget {
     Key? key,
     required this.maxRadius,
     this.child,
-  }) : clipRectSize = 2.0 * (maxRadius / math.sqrt2),
-       super(key: key);
+  })  : clipRectSize = 2.0 * (maxRadius / math.sqrt2),
+        super(key: key);
 
   final double maxRadius;
   final clipRectSize;
@@ -71,13 +71,15 @@ class RadialExpansion extends StatelessWidget {
 class RadialExpansionDemo extends StatelessWidget {
   static const double kMinRadius = 32.0;
   static const double kMaxRadius = 128.0;
-  static const opacityCurve = const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
+  static const opacityCurve =
+      const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
 
   static RectTween _createRectTween(Rect? begin, Rect? end) {
     return MaterialRectCenterArcTween(begin: begin, end: end);
   }
 
-  static Widget _buildPage(BuildContext context, String imageName, String description) {
+  static Widget _buildPage(
+      BuildContext context, String imageName, String description) {
     return Container(
       color: Theme.of(context).canvasColor,
       child: Center(
@@ -116,7 +118,8 @@ class RadialExpansionDemo extends StatelessWidget {
     );
   }
 
-  Widget _buildHero(BuildContext context, String imageName, String description) {
+  Widget _buildHero(
+      BuildContext context, String imageName, String description) {
     return Container(
       width: kMinRadius * 2.0,
       height: kMinRadius * 2.0,
@@ -130,16 +133,17 @@ class RadialExpansionDemo extends StatelessWidget {
             onTap: () {
               Navigator.of(context).push(
                 PageRouteBuilder<void>(
-                  pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+                  pageBuilder: (BuildContext context,
+                      Animation<double> animation,
+                      Animation<double> secondaryAnimation) {
                     return AnimatedBuilder(
-                      animation: animation,
-                      builder: (BuildContext context, Widget? child) {
-                        return Opacity(
-                          opacity: opacityCurve.transform(animation.value),
-                          child: _buildPage(context, imageName, description),
-                        );
-                      }
-                    );
+                        animation: animation,
+                        builder: (BuildContext context, Widget? child) {
+                          return Opacity(
+                            opacity: opacityCurve.transform(animation.value),
+                            child: _buildPage(context, imageName, description),
+                          );
+                        });
                   },
                 ),
               );
